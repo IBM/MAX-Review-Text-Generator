c5c4dd9634f61f92b1bdbb75442a4841062ca58a
Brendan Dwyer <brendan.dwyer@ibm.com>
2018-10-25 12:31:40 -0700

    Test other model endpoints - Removed predict_response() from api/model.py (closes #12) - Added test for other model endpoints - Improved .travis.yml spacing - Increased Travis-CI sleep time to 30 - Added --rm flag to Traivs to keep our internal Jenkins server cleaner - Verify downloaded files MD5 hashes

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,34 @@ import pytest
 import requests
 
 
-def test_response():
+def test_swagger():
+
+    model_endpoint = 'http://localhost:5000/swagger.json'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+    assert r.headers['Content-Type'] == 'application/json'
+
+    json = r.json()
+    assert 'swagger' in json
+    assert json.get('info') and json.get('info').get('title') == 'Model Asset Exchange Server'
+
+
+def test_metadata():
+
+    model_endpoint = 'http://localhost:5000/model/metadata'
+
+    r = requests.get(url=model_endpoint)
+    assert r.status_code == 200
+
+    metadata = r.json()
+    assert metadata['id'] == 'generative-language-model-keras'
+    assert metadata['name'] == 'Generative Language Model Keras'
+    assert metadata['description'] == 'Generative Language Model in Keras trained on Yelp reviews'
+    assert metadata['license'] == 'Apache2'
+
+
+def test_predict():
     seed_text = "went there for dinner on a friday night and i have to say i'm impressed by the quality of the food "
     chars = 20
     model_endpoint = 'http://localhost:5000/model/predict'
